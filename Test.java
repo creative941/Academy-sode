cd07e418ef922d0a949e1db82c8e7ecbc229ab26
creative941 <101525879+creative941@users.noreply.github.com>
2022-03-27 20:20:24 +0300

    homework lesson3

---
+++ Test.java
@@ -0,0 +1,18 @@
+package myfirstpackage;
+
+
+
+public class Test {
+    public static void main(String[] args) {
+     String myName = "Georgi";
+        System.out.println(myName.length());
+        System.out.println(myName.toUpperCase());
+        System.out.println(myName.toLowerCase());
+        System.out.println(16 + 18);
+        System.out.println(Math.abs(-147));
+        System.out.println(Math.abs(15));
+        System.out.println(Math.abs(0));
+        System.out.println(Math.abs(18));
+
+    }
+}
